2e1f9ca18c854c61898979586f5d66f5322f05fc
archit5793 <archit5793@gmail.com>
2017-09-07 23:58:45 -0400

    Improved

--- archit/AS1/AS1/MainWindow.xaml.cs
+++ archit/AS1/AS1/MainWindow.xaml.cs
@@ -25,35 +25,7 @@ namespace AS1
         {
             InitializeComponent();
         }
-        
-        //Function to display ace message
-        private void display_ace(object sender, RoutedEventArgs e)
-        {
-            showText.Content = "Ace of Spades";
-        }
 
-        //Function to display jack message
-        private void display_jack(object sender, RoutedEventArgs e)
-        {
-            showText.Content = "Jack of Clubs";
-        }
-
-        //Function to display joker message
-        private void display_joker(object sender, RoutedEventArgs e)
-        {
-            showText.Content = "A Black Joker";
-        }
-
-        //Function to display king message
-        private void display_king(object sender, RoutedEventArgs e)
-        {
-            showText.Content = "King of Diamonds";
-        }
-
-        //Function to display queen message
-        private void display_queen(object sender, RoutedEventArgs e)
-        {
-            showText.Content = "Queen of Hearts";
-        }
+        private void display_text(object sender, RoutedEventArgs e) => showText.Content = ((Button)sender).Tag;
     }
 }
