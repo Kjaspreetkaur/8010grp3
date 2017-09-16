6839878c20b439c141363af2bb05f708c36fdb55
jeromems <31662932+jeromems@users.noreply.github.com>
2017-09-16 19:25:10 -0400

    Add files via upload

---
+++ AS2_tempConverter/ConvertTemp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace AS2_tempConverter
+{
+    public class ConvertTemp : INotifyPropertyChanged
+    {
+        double _temp;
+        double _tempconversion;
+
+        //gets & sets the value of temperature to be converted
+        public double Temp
+        {
+            get { return _temp; }
+            set { _temp = value; NotifyPropertyChanged(); }
+        }
+        
+        //Convert from Fahrenheit to Celsius
+        public double ConvertToCelsius()
+        {
+            return Math.Round((_temp - 32) * 0.5555556,1);
+        }
+
+        //Convert from Celsius to Fahrenheit
+        public double ConvertToFahrenheit()
+        {
+            return (_temp * 1.8) + 32;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AS2_tempConverter/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AS2_tempConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConvertTemp converttemp;
+        public MainWindow()
+        {
+            InitializeComponent();
+            converttemp = new ConvertTemp();
+            DataContext = converttemp;
+        }
+
+        private void btnCelsius_Click(object sender, RoutedEventArgs e)
+        {
+            //converttemp.ConvertToCelsius();
+            tblkResult.Text = converttemp.ConvertToCelsius().ToString() + "°C";
+        }
+
+        private void btnFahrenheit_Click(object sender, RoutedEventArgs e)
+        {
+            //converttemp.ConvertToFahrenheit();
+            tblkResult.Text = converttemp.ConvertToFahrenheit().ToString() + "°F";
+        }
+    }
+}
