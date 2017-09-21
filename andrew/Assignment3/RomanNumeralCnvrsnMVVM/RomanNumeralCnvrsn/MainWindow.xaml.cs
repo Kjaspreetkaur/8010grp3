a4458b441af8daddbac29a60852a4536a1a70729
ahuynh <nytrickacid@gmail.com>
2017-09-21 11:08:23 -0400

    Added assignment 3 with MVVM implemented

---
+++ andrew/Assignment3/RomanNumeralCnvrsnMVVM/RomanNumeralCnvrsn/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RomanNumeralCnvrsn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RomanNumeralCnvrsnVM rmcVM = new RomanNumeralCnvrsnVM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            rmcVM.integerNumeral = 1;
+            rmcVM.finalString = "";
+            /*
+            int integer = 0;
+
+            if (int.TryParse(tbxGetInt.Text, out integer))
+            {
+                rmcVM.integerNumeral = integer;
+            }
+            rmcVM.finalString = ""; */
+            DataContext = rmcVM;
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            int integer = 0;
+
+            if (int.TryParse(tbxGetInt.Text, out integer))
+            {
+                if (integer <= 0 || integer > 100)
+                {
+                    MessageBox.Show("Please Enter a positive integer between 1-100.");
+                }
+                else
+                {
+                    rmcVM.integerNumeral = integer;
+                    rmcVM.finalString = rmcVM.Convert(integer);
+                    imgTitus.Visibility = Visibility.Visible;
+                    //lblRoman.Content = rmcVM.finalString;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer between 1-100.");
+            }
+        }
+
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            lblRoman.Content = "";
+            tbxGetInt.Text = "";
+            imgTitus.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ andrew/Assignment3/RomanNumeralCnvrsnMVVM/RomanNumeralCnvrsn/RomanNumeralCnvrsnVM.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+//undo has gone too far
+namespace RomanNumeralCnvrsn
+{
+    public class RomanNumeralCnvrsnVM : INotifyPropertyChanged
+    {
+
+
+        private int _integerNumeral = 0;
+        private string _finalRomanNumeral = "";
+
+        public int integerNumeral
+        {
+            get
+            {
+                return _integerNumeral;
+            }
+
+            set
+            {
+                _integerNumeral = value; NotifyPropertyChanged(); //notify XAML that the value has been changed
+            }
+        }
+
+        public String finalString
+        {
+            get
+            {
+                return _finalRomanNumeral;
+            }
+            set
+            {
+                _finalRomanNumeral = value; NotifyPropertyChanged();
+            }
+        }
+
+        public string Convert(int numeral)
+        {
+            
+            int integerNumeral = numeral;
+            string romanNumeral = "";
+
+            if (integerNumeral >= 100)
+            {
+                romanNumeral = romanNumeral + "C";
+                integerNumeral = integerNumeral - 100;
+            }
+            if (integerNumeral >= 90)
+            {
+                romanNumeral = romanNumeral + "XC";
+                integerNumeral = integerNumeral - 90;
+            }
+            if (integerNumeral >= 50)
+            {
+                romanNumeral = romanNumeral + "L";
+                integerNumeral = integerNumeral - 50;
+            }
+            if (integerNumeral >= 40)
+            {
+                romanNumeral = romanNumeral + "XL";
+                integerNumeral = integerNumeral - 40;
+            }
+            while (integerNumeral >= 10)
+            {
+                romanNumeral = romanNumeral + "X";
+                integerNumeral = integerNumeral - 10;
+            }
+            while (integerNumeral >= 9)
+            {
+                romanNumeral = romanNumeral + "IX";
+                integerNumeral = integerNumeral - 9;
+            }
+            while (integerNumeral >= 5)
+            {
+                romanNumeral = romanNumeral + "V";
+                integerNumeral = integerNumeral - 5;
+            }
+            while (integerNumeral >= 4)
+            {
+                romanNumeral = romanNumeral + "IV";
+                integerNumeral = integerNumeral - 4;
+            }
+            while (integerNumeral >= 1)
+            {
+                romanNumeral = romanNumeral + "I";
+                integerNumeral = integerNumeral - 1;
+            }
+            return romanNumeral;
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null) //somebody is listening? yes the xaml
+            {
+                //call them, and tell them something has been changed
+                //we pass in the name of the property that is changed
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
