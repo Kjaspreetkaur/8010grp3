95e09021d6c2abacf5b9368d435f8278015abcdb
jeromems <31662932+jeromems@users.noreply.github.com>
2017-10-07 10:25:03 -0400

    Added Assignment 5

---
+++ jerome/AS5_DiceSimulator/DiceSimulator/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace DiceSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RollDiceVM rollDice = new RollDiceVM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = rollDice;
+        }
+
+        const string DIR_NAME = "PROG8010-AS5";
+        private void btnRollDice_Click(object sender, RoutedEventArgs e)
+        {
+            rollDice.RollDice();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string fullpath = System.IO.Path.Combine(path, DIR_NAME);
+            Directory.CreateDirectory(fullpath);
+            File.AppendAllText(fullpath + @"\x.txt", rollDice.Number1 + " / " + rollDice.Number2 + "\r\n");
+        }
+    }
+}
