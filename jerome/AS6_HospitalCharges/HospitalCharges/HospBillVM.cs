7cf30115305a526cb0c6ea305ff795b7d016a4c2
jeromems <31662932+jeromems@users.noreply.github.com>
2017-10-15 15:58:15 -0400

    Added Assignmet 6

---
+++ jerome/AS6_HospitalCharges/HospitalCharges/HospBillVM.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HospitalCharges
+{
+    class HospBillVM : INotifyPropertyChanged
+    {
+        const decimal DAY_RATE = 350;
+        
+        private decimal days;
+        public decimal Days
+        {
+            get { return days; }
+            set { days = value; NotifyPropertyChanged(); }
+        }
+
+        private decimal meds;
+        public decimal Meds
+        {
+            get { return meds; }
+            set { meds = value; NotifyPropertyChanged(); }
+        }
+
+        private decimal surgery;
+        public decimal Surgery
+        {
+            get { return surgery; }
+            set { surgery = value; NotifyPropertyChanged(); }
+        }
+
+        private decimal lab;
+        public decimal Lab
+        {
+            get { return lab; }
+            set { lab = value; NotifyPropertyChanged(); }
+        }
+
+        private decimal rehab;
+        public decimal Rehab
+        {
+            get { return rehab; }
+            set { rehab = value; NotifyPropertyChanged(); }
+        }
+
+        private decimal baseCharges;
+        public decimal BaseCharges
+        {
+            get { return baseCharges; }
+            set { baseCharges = value; NotifyPropertyChanged(); }
+        }
+
+        public void CalcStayCharges()
+        {
+            BaseCharges = Days * DAY_RATE;
+        }
+
+        private decimal miscCharges;
+        public decimal MiscCharges
+        {
+            get { return miscCharges; }
+            set { miscCharges = value; NotifyPropertyChanged(); }
+        }
+
+        public void CalcMiscCharges()
+        {
+            MiscCharges = Meds + Surgery + Lab + Rehab;
+        }
+
+        private decimal totalCharges;
+        public decimal TotalCharges
+        {
+            get { return totalCharges; }
+            set { totalCharges = value; NotifyPropertyChanged(); }
+        }
+
+        public void CalcTotalCharges()
+        {
+            TotalCharges = BaseCharges + MiscCharges;
+        }
+
+        private DateTime admDate;
+        public DateTime AdmDate
+        {
+            get { return admDate; }
+            set { admDate = value; NotifyPropertyChanged(); }
+        }
+
+        public void SetAdmDate()
+        {
+            int admDays = Convert.ToInt32(Days) * -1;
+            AdmDate = DateTime.Today.AddDays(admDays);
+        }
+
+        #region
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ jerome/AS6_HospitalCharges/HospitalCharges/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace HospitalCharges
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HospBillVM hospChargesVm = new HospBillVM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = hospChargesVm;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tblkAdmDate.Text = " " + DateTime.Now.ToString("yyyy-MM-dd");
+            tblkDischgDate.Text = " " + DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        //Select text in textboxes using tab key
+        private void TextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            ((TextBox)sender).SelectAll();
+        }
+        //Select text in textboxes using mouse click
+        private void Mouse_GotMouseOver(object sender, MouseEventArgs e)
+        {
+            ((TextBox)sender).SelectAll();
+        }
+
+        //Validate text entered in text boxes
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.-]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        //Perform calculation on current textbox 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string tbxName = ((TextBox)sender).Name;
+            switch (tbxName)
+            {
+                case "tbxDays":
+                    hospChargesVm.CalcStayCharges();
+                    hospChargesVm.SetAdmDate();
+                    break;
+                case "tbxMeds": case "tbxSurgery": case "tbxLab": case "tbxRehab":
+                    hospChargesVm.CalcMiscCharges();
+                    break;
+            }
+            hospChargesVm.CalcTotalCharges();
+        }
+    }
+}
