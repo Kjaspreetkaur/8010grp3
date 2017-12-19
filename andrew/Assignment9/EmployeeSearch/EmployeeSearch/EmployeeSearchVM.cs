9c21db97a4758c4a5880c9dec4cdaac6ee868e25
ahuynh <nytrickacid@gmail.com>
2017-12-19 09:25:41 -0500

    Added EmployeeSearch

---
+++ andrew/Assignment9/EmployeeSearch/EmployeeSearch/EmployeeSearchVM.cs
@@ -0,0 +1,72 @@
+﻿/** Assignment Week 10/11 - Employee Search
+ * Archit A.    8024168
+ * Parthik M.   8050213
+ * Jerome S.    8055907
+ * Jaspreet K.  8051666
+ * Andrew H.    8113730
+ * Bhumi S.     8022584
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EmployeeSearch
+{
+    public class EmployeeSearchVM : INotifyPropertyChanged
+    {
+        private List<string> _employeeList = new List<string>();
+        private static List<string> _holdList = new List<string>();
+        private string _searchQuery;
+
+        public List<string> EmployeeList
+        {
+            get
+            {
+                return _employeeList;
+            }
+            set
+            {
+                _employeeList = value;
+                NotifyPropertyChanged();
+
+            }
+        }
+
+        public List<string> HoldList
+        {
+            get;set;
+        }
+
+        public string SearchQuery
+        {
+            get => _searchQuery;
+            set
+            {
+                _searchQuery = value.ToLower();
+                NotifyPropertyChanged();
+                Search();
+            }
+        }
+
+        public void Search()
+        {
+            List<string> temp = new List<string>();
+            foreach (string item in HoldList)
+            {
+                if (item.ToLower().Contains(SearchQuery))
+                {
+                    temp.Add(item);
+                }
+            }
+            EmployeeList = temp;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ andrew/Assignment9/EmployeeSearch/EmployeeSearch/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿/** Assignment Week 10/11 - Employee Search
+ * Archit A.    8024168
+ * Parthik M.   8050213
+ * Jerome S.    8055907
+ * Jaspreet K.  8051666
+ * Andrew H.    8113730
+ * Bhumi S.     8022584
+ */
+using System.Collections.Generic;
+using System.Windows;
+using System.Data.SqlClient;
+
+namespace EmployeeSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmployeeSearchVM esvm = new EmployeeSearchVM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = esvm;
+            esvm.HoldList = PopulateList();
+            esvm.EmployeeList = esvm.HoldList;
+        }
+
+        public List<string> PopulateList()
+        {
+            const string CONNECT_STRING = @"Server=.\SQLEXPRESS;Database=Personnel.db3;Trusted_Connection=True;";
+            List<string> temp = new List<string>();
+            SqlConnection conn = new SqlConnection(CONNECT_STRING);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT * FROM EMPLOYEE"; 
+            cmd.ExecuteNonQuery(); 
+
+            SqlDataReader dr = cmd.ExecuteReader(); 
+
+            while (dr.Read())
+            {
+                temp.Add(dr.GetString(1).ToString() + " " +
+                         dr.GetString(2).ToString() + " " +
+                         dr.GetString(3).ToString() + " ");
+            }
+            dr.Close();
+
+            return temp;
+        }
+    }
+}
